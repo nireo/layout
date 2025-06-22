be477b3d8f829a5ba1d72c60b5446d30e0f20fa5
nireo <eemil.lehtikangas1@gmail.com>
2025-06-22 22:32:04 +0300

    fixed some layers and better layouts etc

--- keymap.c
+++ keymap.c
@@ -40,42 +40,25 @@
 #define RIV_TAG7 G(KC_7)       // Super+7: tag 7
 #define RIV_TAG8 G(KC_8)       // Super+8: tag 8
 #define RIV_TAG9 G(KC_9)       // Super+9: tag 9
-
-#define KC_EXLM S(KC_1)    // !
-#define KC_AT S(KC_2)      // @
-#define KC_HASH S(KC_3)    // #
-#define KC_DLR S(KC_4)     // $
-#define KC_PERC S(KC_5)    // %
-#define KC_CIRC S(KC_6)    // ^
-#define KC_AMPR S(KC_7)    // &
-#define KC_ASTR S(KC_8)    // *
-#define KC_LPRN S(KC_9)    // (
-#define KC_RPRN S(KC_0)    // )
-#define KC_LCBR S(KC_LBRC) // {
-#define KC_RCBR S(KC_RBRC) // }
-#define KC_LABK S(KC_COMM) //
-#define KC_RABK S(KC_DOT)  // >
-#define KC_QUES S(KC_SLSH) // ?
-#define KC_DQUO S(KC_QUOT) // "
-#define KC_COLN S(KC_SCLN) // :
-#define KC_TILD S(KC_GRV)  // ~
+#define VIM_U C(KC_U)          // Ctrl+U: page up in vim
+#define VIM_D C(KC_D)          // Ctrl+D: page down in vim
 
 enum layers {
-  _DEF,
-  _SYM,
-  _NAV,
-  _RIV, // New River window management layer
-  _NUM,
+    _DEF,
+    _SYM,
+    _NAV,
+    _RIV, // New River window management layer
+    _NUM,
 };
 
 enum keycodes {
-  // Custom oneshot mod implementation with no timers.
-  OS_SHFT = SAFE_RANGE,
-  OS_CTRL,
-  OS_ALT,
-  OS_CMD,
-  SW_WIN, // Switch to next window (alt-tab) - kept for compatibility
-  SW_TAB, // Switch to next browser tab (ctrl-tab)
+    // Custom oneshot mod implementation with no timers.
+    OS_SHFT = SAFE_RANGE,
+    OS_CTRL,
+    OS_ALT,
+    OS_CMD,
+    SW_WIN, // Switch to next window (alt-tab) - kept for compatibility
+    SW_TAB, // Switch to next browser tab (ctrl-tab)
 };
 
 // clang-format off
@@ -95,13 +78,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_NAV] = LAYOUT_split_3x5_3(
-        SW_TAB,  SW_WIN,  TAB_L,   TAB_R,   KC_ESC,  KC_ESC,  KC_HOME, KC_END,  KC_BSPC, KC_DEL,
-        OS_CMD,  OS_ALT,  OS_CTRL, OS_SHFT, KC_ENT,  KC_ENT,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
+        SW_TAB,  SW_WIN,  TAB_L,   TAB_R,   VIM_D,  VIM_U,  KC_HOME, KC_UP,  KC_BSPC, KC_DEL,
+        OS_CMD,  OS_ALT,  OS_CTRL, OS_SHFT, KC_ENT,  KC_ENT,  KC_LEFT, KC_DOWN, KC_RGHT,   KC_END,
         SPACE_L, SPACE_R, C(KC_Z), C(KC_Y), KC_TAB,  KC_TAB,  KC_PGUP, KC_PGDN, KC_QUOT, KC_NUM,
         _______, _______, _______, _______, _______, _______
     ),
 
-    // New River window management layer
     [_RIV] = LAYOUT_split_3x5_3(
         RIV_TAG1, RIV_TAG2, RIV_TAG3, RIV_TAG4, RIV_TAG5, RIV_TAG6, RIV_TAG7, RIV_TAG8, RIV_TAG9, RIV_CLOSE,
         RIV_LAUNCHER, RIV_LAST, RIV_FOCUS_PREV, RIV_FOCUS_NEXT, RIV_FLOAT, RIV_FULL, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,
@@ -110,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_NUM] = LAYOUT_split_3x5_3(
-        KC_P1,   KC_P2,   KC_P3,   KC_P4,   KC_P5,   KC_P6,   KC_P7,   KC_P8, KC_P9,     KC_P0,    // !
+        KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,  KC_9,     KC_0,    // !
         OS_CMD,  OS_ALT,  OS_CTRL, OS_SHFT, KC_F11,  KC_F12,  OS_SHFT, OS_CTRL, OS_ALT,  OS_CMD,
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
         _______, _______, _______, _______, _______, _______
@@ -119,30 +101,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // clang-format on
 
 bool is_oneshot_cancel_key(uint16_t keycode) {
-  switch (keycode) {
-  case LA_SYM:
-  case LA_NAV:
-  case LA_RIV:
-    return true;
-  default:
-    return false;
-  }
+    switch (keycode) {
+        case LA_SYM:
+        case LA_NAV:
+        case LA_RIV:
+            return true;
+        default:
+            return false;
+    }
 }
 
 bool is_oneshot_ignored_key(uint16_t keycode) {
-  switch (keycode) {
-  case LA_SYM:
-  case LA_NAV:
-  case LA_RIV:
-  case KC_LSFT:
-  case OS_SHFT:
-  case OS_CTRL:
-  case OS_ALT:
-  case OS_CMD:
-    return true;
-  default:
-    return false;
-  }
+    switch (keycode) {
+        case LA_SYM:
+        case LA_NAV:
+        case LA_RIV:
+        case KC_LSFT:
+        case OS_SHFT:
+        case OS_CTRL:
+        case OS_ALT:
+        case OS_CMD:
+            return true;
+        default:
+            return false;
+    }
 }
 
 bool sw_win_active = false;
@@ -150,23 +132,21 @@ bool sw_tab_active = false;
 
 oneshot_state os_shft_state = os_up_unqueued;
 oneshot_state os_ctrl_state = os_up_unqueued;
-oneshot_state os_alt_state = os_up_unqueued;
-oneshot_state os_cmd_state = os_up_unqueued;
+oneshot_state os_alt_state  = os_up_unqueued;
+oneshot_state os_cmd_state  = os_up_unqueued;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  update_swapper(&sw_win_active, KC_LALT, KC_TAB, SW_WIN, OS_SHFT, keycode,
-                 record);
-  update_swapper(&sw_tab_active, KC_LCTL, KC_TAB, SW_TAB, OS_SHFT, keycode,
-                 record);
+    update_swapper(&sw_win_active, KC_LALT, KC_TAB, SW_WIN, OS_SHFT, keycode, record);
+    update_swapper(&sw_tab_active, KC_LCTL, KC_TAB, SW_TAB, OS_SHFT, keycode, record);
 
-  update_oneshot(&os_shft_state, KC_LSFT, OS_SHFT, keycode, record);
-  update_oneshot(&os_ctrl_state, KC_LCTL, OS_CTRL, keycode, record);
-  update_oneshot(&os_alt_state, KC_LALT, OS_ALT, keycode, record);
-  update_oneshot(&os_cmd_state, KC_LCMD, OS_CMD, keycode, record);
+    update_oneshot(&os_shft_state, KC_LSFT, OS_SHFT, keycode, record);
+    update_oneshot(&os_ctrl_state, KC_LCTL, OS_CTRL, keycode, record);
+    update_oneshot(&os_alt_state, KC_LALT, OS_ALT, keycode, record);
+    update_oneshot(&os_cmd_state, KC_LCMD, OS_CMD, keycode, record);
 
-  return true;
+    return true;
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _SYM, _NAV, _NUM);
+    return update_tri_layer_state(state, _SYM, _NAV, _NUM);
 }
