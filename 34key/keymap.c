c20da21f666bd0ebd17c6b89a896c03ab3b91de7
nireo <eemil.lehtikangas1@gmail.com>
2025-09-05 00:09:58 +0300

    added some window manager bindings to the 34 key layout

--- 34key/keymap.c
+++ 34key/keymap.c
@@ -50,15 +50,15 @@
 enum layers { _DEF, _SYM, _NAV, _NUM };
 
 enum keycodes {
-    // Custom oneshot mod implementation with no timers.
-    OS_SHFT = SAFE_RANGE,
-    OS_CTRL,
-    OS_ALT,
-    OS_CMD,
-    SW_WIN, // Switch to next window (alt-tab) - kept for compatibility
-    SW_TAB, // Switch to next browser tab (ctrl-tab)
-    // River WM shortcuts accessed via tap-hold
-    RIV_SPC, // Space/River layer
+  // Custom oneshot mod implementation with no timers.
+  OS_SHFT = SAFE_RANGE,
+  OS_CTRL,
+  OS_ALT,
+  OS_CMD,
+  SW_WIN, // Switch to next window (alt-tab) - kept for compatibility
+  SW_TAB, // Switch to next browser tab (ctrl-tab)
+  // River WM shortcuts accessed via tap-hold
+  RIV_SPC, // Space/River layer
 };
 
 // clang-format off
@@ -86,36 +86,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_NUM] = LAYOUT_split_3x5_2(
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-        KC_MPRV, KC_MNXT, KC_MSTP, KC_MPLY, KC_VOLU, KC_F12,  OS_SHFT, OS_CTRL, OS_ALT,  OS_CMD,
-        KC_KB_VOLUME_DOWN, KC_KB_VOLUME_UP, KC_VOLD, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
+        KC_MPRV, KC_MNXT, KC_MSTP, KC_MPLY, RIV_FOCUS_PREV, RIV_FOCUS_NEXT,  OS_SHFT, OS_CTRL, OS_ALT,  OS_CMD,
+        KC_KB_VOLUME_DOWN, KC_KB_VOLUME_UP, RIV_LAUNCHER, RIV_RATIO_DEC, RIV_RATIO_INC, RIV_TERM, RIV_CLOSE, KC_F8, KC_F9, KC_F10,
                                    _______, _______, _______, _______
     )
 };
 // clang-format on
 
 bool is_oneshot_cancel_key(uint16_t keycode) {
-    switch (keycode) {
-        case LA_SYM:
-        case LA_NAV:
-            return true;
-        default:
-            return false;
-    }
+  switch (keycode) {
+  case LA_SYM:
+  case LA_NAV:
+    return true;
+  default:
+    return false;
+  }
 }
 
 bool is_oneshot_ignored_key(uint16_t keycode) {
-    switch (keycode) {
-        case LA_SYM:
-        case LA_NAV:
-        case KC_LSFT:
-        case OS_SHFT:
-        case OS_CTRL:
-        case OS_ALT:
-        case OS_CMD:
-            return true;
-        default:
-            return false;
-    }
+  switch (keycode) {
+  case LA_SYM:
+  case LA_NAV:
+  case KC_LSFT:
+  case OS_SHFT:
+  case OS_CTRL:
+  case OS_ALT:
+  case OS_CMD:
+    return true;
+  default:
+    return false;
+  }
 }
 
 bool sw_win_active = false;
@@ -123,21 +123,23 @@ bool sw_tab_active = false;
 
 oneshot_state os_shft_state = os_up_unqueued;
 oneshot_state os_ctrl_state = os_up_unqueued;
-oneshot_state os_alt_state  = os_up_unqueued;
-oneshot_state os_cmd_state  = os_up_unqueued;
+oneshot_state os_alt_state = os_up_unqueued;
+oneshot_state os_cmd_state = os_up_unqueued;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    update_swapper(&sw_win_active, KC_LALT, KC_TAB, SW_WIN, OS_SHFT, keycode, record);
-    update_swapper(&sw_tab_active, KC_LCTL, KC_TAB, SW_TAB, OS_SHFT, keycode, record);
+  update_swapper(&sw_win_active, KC_LALT, KC_TAB, SW_WIN, OS_SHFT, keycode,
+                 record);
+  update_swapper(&sw_tab_active, KC_LCTL, KC_TAB, SW_TAB, OS_SHFT, keycode,
+                 record);
 
-    update_oneshot(&os_shft_state, KC_LSFT, OS_SHFT, keycode, record);
-    update_oneshot(&os_ctrl_state, KC_LCTL, OS_CTRL, keycode, record);
-    update_oneshot(&os_alt_state, KC_LALT, OS_ALT, keycode, record);
-    update_oneshot(&os_cmd_state, KC_LCMD, OS_CMD, keycode, record);
+  update_oneshot(&os_shft_state, KC_LSFT, OS_SHFT, keycode, record);
+  update_oneshot(&os_ctrl_state, KC_LCTL, OS_CTRL, keycode, record);
+  update_oneshot(&os_alt_state, KC_LALT, OS_ALT, keycode, record);
+  update_oneshot(&os_cmd_state, KC_LCMD, OS_CMD, keycode, record);
 
-    return true;
+  return true;
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    return update_tri_layer_state(state, _SYM, _NAV, _NUM);
+  return update_tri_layer_state(state, _SYM, _NAV, _NUM);
 }
